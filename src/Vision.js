ca10e935b7b5fea1746c2b3e13f8a9eaf3f7a1e8
steninxavier <steninxavier@gmail.com>
2021-11-06 11:31:32 +0100

    form value

--- src/Vision.js
+++ src/Vision.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
 
 
 function Vision() {
 
-    axios({
-        method: 'post',
-        url: 'https://cors.bridged.cc/https://api.openvisionapi.com/api/v1/detection',
-        data: {
-          image: ''
-        }
-      }).then(Request=>{
-          console.log(Request)
-      })
-    
+    const[image,setImage]= useState(null)
+
+      const imageSelector= (e)=>{
+          console.log(e.target.value)
+          setImage(e.target.value)
+      }
+
+       
     return (
         <div>
             <form  method="post" encType="multipart/form-data">
-            <input type="file" placeholder="upload an image" id="image" required/>
+            <input type="file" placeholder="upload an image" onChange={imageSelector} id="image" required/>
             <Button variant="outline-success" type="submit" >UPLOAD</Button>
+            {image}
             </form>
         </div>
     )
